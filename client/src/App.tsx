ae13bad7386c75b18237188161eb680072c3eb2b
peebaKU <baramee.254450@gmail.com>
2023-11-11 10:31:07 +0700

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Baramee</h1>
+        <button className="btn btn-primary">click</button>
+      </div>
+    </>
+  )
+}
+
+export default App
